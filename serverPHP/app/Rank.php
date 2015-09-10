86920cdc249f77dcd53add2fd89161e0be0e3618
qgx446738721 <qgx446738721@hotmail.com>
2015-09-10 18:09:08 +0800

    构建模型
    rank 模型

---
+++ serverPHP/app/Rank.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dog
+ * Date: 2015/9/10
+ * Time: 18:00
+ */
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rank extends Model{
+}
